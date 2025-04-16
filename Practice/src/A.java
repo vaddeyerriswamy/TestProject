34b9b19e6e678f8e83fac34e73a991f19d123da7
vaddeyerriswamy <vyswamy465@gmail.com>
2025-04-16 21:32:33 +0530

    Adding Program

---
+++ Practice/src/A.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+public class A {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {111, 13, 25, 9, 34, 1};
+			Arrays.sort(a);
+			
+			System.out.println(a[a.length-2]);
+			System.out.println(a[a.length-5]);
+			
+			int max=a[0];
+			int min=a[0];
+			
+			for(int i=0;i<a.length;i++) {
+				if(max<a[i]) {
+					max=a[i];
+				}else if(min>a[i]) {
+					min=a[i];
+				}
+			}
+			System.out.println(max);
+			String str ="infosys";
+			//	output="InFoSyS"
+			StringBuffer sb= new StringBuffer();
+			for(int i=0;i<str.length();i++) {
+				char ch=str.charAt(i);
+					if(i%2==0) {
+						sb.append(Character.toUpperCase(ch));
+					}else {
+						sb.append(Character.toLowerCase(ch));
+					}
+			}
+			System.out.println(sb);
+
+		
+		
+	}
+
+}
